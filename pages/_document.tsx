d2b141ac9b32220407a07e13ab18ccf5ca7a187e
seobbang <seobbangway@naver.com>
2023-05-28 19:54:00 +0900

    init: 폴더구조 세팅

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
+// app 다음에 실행됨.
+// 공통적으로 활용할 <head>, <body> 태그 안에 들어갈 내용들을 커스텀
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>깃허브 맞팔 탐지기</title>
+        <meta charSet="utf-8"></meta>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
