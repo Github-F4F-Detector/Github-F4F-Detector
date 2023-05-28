4abd977a602dce2bb48f2660e02b0fcf70c7c374
seobbang <seobbangway@naver.com>
2023-05-28 19:59:40 +0900

    init: react-query 설치

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import type { AppProps } from "next/app";
+import { QueryClient } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
 
 // 서버로 요청이 들어왔을 때 가장 먼저 실행되는 컴포넌트.
 // 공통 레이아웃 적용
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
