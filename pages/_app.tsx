2b156b7d46395158dea8787c57741a10d2182874
seobbang <seobbangway@naver.com>
2023-05-30 11:03:33 +0900

    init: eslint import 플러그인 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
-import type { AppProps } from 'next/app';
-import { QueryClient } from '@tanstack/react-query';
 import { QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient } from '@tanstack/react-query';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { ThemeProvider } from 'styled-components';
+
 import GlobalStyle from '@/styles/globalStyle';
 import { theme } from '@/styles/theme';
-import Head from 'next/head';
 
 // 서버로 요청이 들어왔을 때 가장 먼저 실행되는 컴포넌트.
 // 공통 레이아웃 적용
