a7af43517dcad44069d71bf2dc7d468581b470b9
seobbang <seobbangway@naver.com>
2023-05-30 11:06:14 +0900

    init: 이미지,아이콘 reactcomponent로 export

--- public/icon/index.ts
+++ public/icon/index.ts
@@ -1,2 +1,2 @@
 // 명명규칙은 xxxxIcon으로 통일
-// export { default as AddressIcon } from './btn_address.svg';
+// export { ReactComponent as AdobeLogoIcon } from './AdobeCorporateLogo1.svg';
--- public/image/index.ts
+++ public/image/index.ts
@@ -1,2 +1,2 @@
 // 명명규칙은 xxxxImg으로 통일
-// export { ReactComponent as AdobeLogo } from './AdobeCorporateLogo1.svg';
+// export { ReactComponent as AdobeLogoImg } from './AdobeCorporateLogo1.svg';
