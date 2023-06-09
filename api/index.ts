70018dd2102cf7d067d5286a6a11994feaec3136
seobbang <seobbangway@naver.com>
2023-06-09 14:46:35 +0900

    feat#5: axios instance header 추가

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,14 @@
 import axios, { AxiosInstance } from 'axios';
+import { useRecoilValue } from 'recoil';
+import { userTokenState } from 'states/user';
 
-export const client: AxiosInstance = axios.create({
-  baseURL: 'https://api.github.com',
-});
+export const Client = () => {
+  const token = useRecoilValue(userTokenState);
+  const client: AxiosInstance = axios.create({
+    baseURL: 'https://api.github.com',
+    headers: {
+      Authorization: `token ${token}`,
+    },
+  });
+  return client;
+};
